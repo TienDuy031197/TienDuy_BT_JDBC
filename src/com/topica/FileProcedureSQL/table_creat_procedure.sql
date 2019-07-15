cbb4eb67140b035383b18f3810032ba2014bda48
unknown <duyco0311@gmail.com>
2019-07-16 00:45:12 +0700

    f

--- src/com/topica/FileProcedureSQL/table_creat_procedure.sql
+++ src/com/topica/FileProcedureSQL/table_creat_procedure.sql
@@ -2,9 +2,9 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `table_creat_procedure`()
 BEGIN
 	Create table IF NOT EXISTS employees(
 		id int(10) NOT NULL auto_increment,
-        name varchar(50) NOT NULL,
-        address varchar(50) NOT NULL,
-        phone varchar(10) NOT NULL,
-       	primary key(id)
+        		name varchar(50) NOT NULL,
+        		address varchar(50) NOT NULL,
+       		phone varchar(10) NOT NULL,
+       		primary key(id)
    	 );
 END
