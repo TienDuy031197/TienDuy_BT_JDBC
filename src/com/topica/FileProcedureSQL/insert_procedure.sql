684fc72641ec5b2e02f558098b3685b1863074d8
unknown <duyco0311@gmail.com>
2019-07-16 00:51:15 +0700

    g

--- src/com/topica/FileProcedureSQL/insert_procedure.sql
+++ src/com/topica/FileProcedureSQL/insert_procedure.sql
@@ -1,7 +1,8 @@
 CREATE DEFINER=`root`@`localhost` PROCEDURE `insert_procedure`(IN _id INTEGER,	
 							  IN _name VARCHAR(50),
 							  IN _address VARCHAR(50),
-                              						IN _phone VARCHAR(10))
+                              						  IN _phone VARCHAR(10))
+
 BEGIN
 	INSERT INTO employees(id,name,address,phone)
    	VALUES(_id,_name,_address,_phone);
